ec2071482956a5eedb325f87c4c7548a3b55c256
SadCat1233 <vac_for_91@mail.ru>
2026-02-21 16:53:20 +0300

    fix task2_wb1_1.cpp

--- algorithm/sem2/KR1/wb1.1/task2_wb1_1.cpp
+++ algorithm/sem2/KR1/wb1.1/task2_wb1_1.cpp
@@ -1,38 +1,45 @@
 #include <iostream>
 #include <vector>
+#include <bitset>
 
 using namespace std;
 
+const int MAX_SUM = 1000000;
+
+
+bool half(const vector<int>& arr, int valuesSum) {
+    if (valuesSum % 2 != 0) return false;
+    int target = valuesSum / 2;
+
+    if (target > MAX_SUM) return false;
+
+    bitset<MAX_SUM + 1> dp;
+    dp[0] = 1;
+
+    for (int number : arr) {
+        dp |= (dp << number);
+    }
+
+    return dp[target];
+}
+
 int main() {
-    int n;
-    int sum = 0;
-    cin >> n;
-    vector<int> nums(n);
+    int num, value;
+    int valuesSum = 0;
 
-    for (int i = 0; i < n; i++) {
-        cin >> nums[i];
-        sum += nums[i];
-    }
+    cin >> num;
+    vector<int> arr(num);
 
-    if (sum % 2 != 0) {
-        cout << "NO" << endl;
-        return 0;
-    }
-
-    int target = sum / 2;
-    vector<bool> dp(target + 1, false);
-    dp[0] = true;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = target; j >= nums[i]; j--) {
-            dp[j] = dp[j] || dp[j - nums[i]];
+    for (int i = 0; i < num; i++) {
+        cin >> value;
+        if (value < 0) { 
+            cout << "NO";
+            return 0;
         }
+        valuesSum += value;
+        arr.push_back(value);
     }
 
-    if (dp[target])
-        cout << "YES";
-    else
-        cout << "NO";
-
+    cout << (half(arr, valuesSum) ? "YES\n" : "NO\n");
     return 0;
 }
